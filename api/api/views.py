151819e1e96d0a713aff2cabaa2625a78638fadd
Jean-Baptiste Marrec <jeanbaptiste.marrec@gmail.com>
2026-05-24 15:58:06 +0200

    feat: add serializers and views

---
+++ api/api/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from models import HealthRecord
+from serializers import HealthRecordSerializer
+
+class HealthRecordViewSet(viewsets.ModelViewSet):
+    queryset = HealthRecord.objects.all()
+    serializer_class = HealthRecordSerializer
