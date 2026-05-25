27ca1486d187f1e67ff8bfa7a2634005551ea117
Jean-Baptiste Marrec <jeanbaptiste.marrec@gmail.com>
2026-05-25 16:23:22 +0200

    fix: correct relative imports in serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import HealthRecord
+from .models import HealthRecord
 
 class HealthRecordSerializer(serializers.ModelSerializer):
     class Meta:
